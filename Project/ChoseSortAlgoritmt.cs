b04a5c186fec391fdfba029d6bee2735969db96e
Konstantin <Konstantin.Kirchanov@yandex.ru>
2022-03-08 23:28:27 +0300

    ChangeFormat

--- Project/ChoseSortAlgoritmt.cs
+++ Project/ChoseSortAlgoritmt.cs
@@ -23,62 +23,62 @@ namespace Cursach
 		
 		int temp;
 		static void Rest(int[] A, int size, int indx)//конец просеивания пирамиды
-        {        
-                int temp, c, s;
-                c = 2 * indx;
-                while ((size > c + 1))
-                {
-                    s = 0;
-                    if (size > c + 2)//если есть 2 потомка
-                    {
-                        if ((A[c + 2] > A[indx]) && (A[c + 1] < A[c + 2]))//проверка перехода в правый потомок
-                        {
-                            temp = A[c + 2];
-                            A[c + 2] = A[indx];
-                            A[indx] = temp;
-                            s = 1;
-                            indx = c + 2;//выписываем индекс участвовавшие в обмене элемента
-                        }
-                        else
-                            if (A[c + 1] > A[indx])//проверка перехода в левый потомок
-                            {
-                                temp = A[c + 1];
-                                A[c + 1] = A[indx];
-                                A[indx] = temp;
-                                s = 1;
-                                indx = c + 1;
-                            }
-                    }
-                    else
-                        if (A[c + 1] > A[indx])//иначе проверяем при наличии одного потомка
-                        {
-                            temp = A[c + 1];
-                            A[c + 1] = A[indx];
-                            A[indx] = temp;
-                            s = 1;
-                            indx = c + 1;
-                        }
-                    if (s == 0)
-                       break;
-                    c = 2 * indx;//проверяем, есть ли потомки у элемента, с которым произошёл обмен
-                }             
-        }	
+        	{        
+               		int temp, c, s;
+                	c = 2 * indx;
+                	while ((size > c + 1))
+                	{
+                    		s = 0;
+                    	if (size > c + 2)//если есть 2 потомка
+                    	{
+                        	if ((A[c + 2] > A[indx]) && (A[c + 1] < A[c + 2]))//проверка перехода в правый потомок
+                        	{
+                            		temp = A[c + 2];
+                            		A[c + 2] = A[indx];
+                            		A[indx] = temp;
+                            		s = 1;
+                            		indx = c + 2;//выписываем индекс участвовавшие в обмене элемента
+                        	}
+                        	else
+                            		if (A[c + 1] > A[indx])//проверка перехода в левый потомок
+                            		{
+                                		temp = A[c + 1];
+                                		A[c + 1] = A[indx];
+                                		A[indx] = temp;
+                                		s = 1;
+                                		indx = c + 1;
+                            		}
+                    	}
+                    	else
+                        	if (A[c + 1] > A[indx])//иначе проверяем при наличии одного потомка
+                        	{
+                            		temp = A[c + 1];
+                            		A[c + 1] = A[indx];
+                            		A[indx] = temp;
+                            		s = 1;
+                            		indx = c + 1;
+                        	}
+                    		if (s == 0)
+                       		break;
+                    		c = 2 * indx;//проверяем, есть ли потомки у элемента, с которым произошёл обмен
+                	}             
+        	}	
 		
 		public int [] Int_Tournir(int [] A)
 		{
 			for (int n = A.Length / 2 - 1; n >= 0; n--)
-            {
-                Rest(A, A.Length, n);//просеивание дерева         
-            }
+            		{
+                		Rest(A, A.Length, n);//просеивание дерева         
+            		}
 
-            for (int j = A.Length - 1; j >= 1; j--)
-            {
-                temp = A[0];
-                A[0] = A[j];
-                A[j] = temp;
-                Rest(A, j, 0);
-            }
-            return A;	            
+            		for (int j = A.Length - 1; j >= 1; j--)
+            		{
+                		temp = A[0];
+                		A[0] = A[j];
+                		A[j] = temp;
+                		Rest(A, j, 0);
+            		}
+            		return A;	            
 		}
 	}
 }
